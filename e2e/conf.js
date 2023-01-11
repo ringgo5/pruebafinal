51455b5af318d08974b9f303adaa1f8cadfaeb79
Vicente Morata <10517452@alu.murciaeduca.es>
2023-01-11 17:23:14 +0100

    protractor,screeshot,allure

--- e2e/conf.js
+++ e2e/conf.js
@@ -1,4 +1,21 @@
 // An example configuration file.
+
+//esto es para screeshot:
+
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
+var reporter = new HtmlScreenshotReporter({
+  dest: 'target/screenshots',
+  filename: 'my-report.html'
+});
+//hasta a aqui----la demas conf de screeshot ira a abajo
+
+//--------conf para Allure----------------
+
+
+//------------hasta aqui la conf de allure
+
+
 exports.config = {
   directConnect: true,
 
@@ -17,5 +34,32 @@ exports.config = {
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
+  }, //ponemos la comita y añadimos todos lo de abajo. 
+  // Setup the report before any tests start
+  beforeLaunch: function () {
+    return new Promise(function (resolve) {
+      reporter.beforeLaunch(resolve);
+    });
+  },
+
+  // Assign the test reporter to each running instance
+  onPrepare: function () {
+    jasmine.getEnv().addReporter(reporter);
+    //a partir de aqui hasta resultsDIr es conf de allure
+    var AllureReporter = require('jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(new AllureReporter({
+      resultsDir: 'allure-results'
+    }));
+  },
+
+  // Close the report after all tests finish
+  afterLaunch: function (exitCode) {
+    return new Promise(function (resolve) {
+      reporter.afterLaunch(resolve.bind(this, exitCode));
+    });
   }
+
+  //Aquí acaba la configuración de screeshot.  No olvidar poner lo de arriba
+
+
 };
