b2312d22b7735826bfcb2061aea592507391e529
Vicente Morata <10517452@alu.murciaeduca.es>
2023-01-11 17:45:19 +0100

    scheenshot y screeshot con html

--- e2e/conf.js
+++ e2e/conf.js
@@ -10,12 +10,6 @@ var reporter = new HtmlScreenshotReporter({
 });
 //hasta a aqui----la demas conf de screeshot ira a abajo
 
-//--------conf para Allure----------------
-
-
-//------------hasta aqui la conf de allure
-
-
 exports.config = {
   directConnect: true,
 
@@ -44,18 +38,81 @@ exports.config = {
 
   // Assign the test reporter to each running instance
   onPrepare: function () {
+
+    //esto lo añadimos como paso 2 de html reports con schreeshot:
+
+    var jasmineReporters = require('jasmine-reporters');
+    jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+      consolidateAll: true,
+      savePath: './',
+      filePrefix: 'xmlresults'
+    }));
+
+    //------hasta aquí html report screenshot------------
+
     jasmine.getEnv().addReporter(reporter);
     //a partir de aqui hasta resultsDIr es conf de allure
     var AllureReporter = require('jasmine-allure-reporter');
     jasmine.getEnv().addReporter(new AllureReporter({
       resultsDir: 'allure-results'
     }));
+
+    //Este coódigo de abajo es para que podamos ver una screenshot dentro del de la web con el reporte de los test
+    //en reportes html con screenshot.
+    var fs = require('fs-extra');
+
+    fs.emptyDir('screenshots/', function (err) {
+      console.log(err);
+    });
+
+    jasmine.getEnv().addReporter({
+      specDone: function (result) {
+        if (result.status == 'failed') {
+          browser.getCapabilities().then(function (caps) {
+            var browserName = caps.get('browserName');
+
+            browser.takeScreenshot().then(function (png) {
+              var stream = fs.createWriteStream('screenshots/' + browserName + '-' + result.fullName + '.png');
+              stream.write(new Buffer(png, 'base64'));
+              stream.end();
+            });
+          });
+        }
+      }
+    });
   },
 
   // Close the report after all tests finish
   afterLaunch: function (exitCode) {
     return new Promise(function (resolve) {
       reporter.afterLaunch(resolve.bind(this, exitCode));
+    });
+  }//ahora aquí introduzco lo necesario para hacer reportes en html con screenshot:
+
+  ,
+  onComplete: function () {
+    var browserName, browserVersion;
+    var capsPromise = browser.getCapabilities();
+
+    capsPromise.then(function (caps) {
+      browserName = caps.get('browserName');
+      browserVersion = caps.get('version');
+      platform = caps.get('platform');
+
+      var HTMLReport = require('protractor-html-reporter-2');
+
+      testConfig = {
+        reportTitle: 'Protractor Test Execution Report',
+        outputPath: './',
+        outputFilename: 'ProtractorTestReport',
+        screenshotPath: './screenshots',
+        testBrowser: browserName,
+        browserVersion: browserVersion,
+        modifiedSuiteName: false,
+        screenshotsOnlyOnFailure: true,
+        testPlatform: platform
+      };
+      new HTMLReport().from('xmlresults.xml', testConfig);
     });
   }
 
